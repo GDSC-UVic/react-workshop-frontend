5ac8ecb1853bbf3a6605fd0fba0be009cc032e2b
Brian Pham <nguyenphamswork@gmail.com>
2024-04-11 22:10:00 -0700

    added useEffect demonstration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(()=> {
+    setCount(5)
+  }, [])
 
   return (
     <>
