164235cab0cea462d185e5c74f8f8c44f02cb3cd
Brian Pham <nguyenphamswork@gmail.com>
2024-04-11 22:26:37 -0700

    added dog picture section of workshop

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("");
 
-  useEffect(()=> {
-    setCount(5)
-  }, [])
+  const fetchUrl = async () => {
+    const response = await fetch(import.meta.env.VITE_APP_SHIBA_URL).then(
+      (res) => res.json()
+    );
+    return response[0];
+  };
+
+  useEffect(() => {
+    const response = fetchUrl();
+    response.then((data) => {
+      setUrl(data);
+    });
+  },[]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+          <img src={url} width={300} alt="Shiba Inu" />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -33,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
