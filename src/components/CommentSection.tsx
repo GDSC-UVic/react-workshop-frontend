7a1617b85ca851a5a3c6892834a3673248edaf79
Brian Pham <nguyenphamswork@gmail.com>
2024-04-15 20:26:07 -0700

    added section 4

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -8,6 +8,8 @@ type Comment = {
 
 const CommentSection = () => {
   const [url, setUrl] = useState([]);
+  const [comment, setComment] = useState("");
+  const [author, setAuthor] = useState("");
 
   const fetchUrl = async () => {
     const response = await fetch(import.meta.env.VITE_APP_COMMENTS_API, {
@@ -17,6 +19,31 @@ const CommentSection = () => {
       },
     }).then((res) => res.json());
     return response;
+  };
+
+  const postComment = async () => {
+    await fetch(import.meta.env.VITE_APP_COMMENTS_API, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_APP_COMMENTS_API_KEY}`,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        comment: comment,
+        author: author,
+      }),
+    })
+      .then(() => {
+        const response = fetchUrl();
+        response.then((data) => {
+          setUrl(data);
+        });
+        setComment("");
+        setAuthor("");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   useEffect(() => {
@@ -30,10 +57,39 @@ const CommentSection = () => {
     <div className="comment-section">
       {url.map((comment: Comment) => (
         <div key={comment.id} className="comment">
-          <p><strong>{comment.author}</strong></p>
+          <p>
+            <strong>{comment.author}</strong>
+          </p>
           <p>{comment.comment}</p>
         </div>
       ))}
+      <form className="form">
+        <label>
+          Comment:
+          <textarea
+          rows={6}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </label>
+        <label>
+          Author
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={async (e) => {
+            e.preventDefault();
+            postComment();
+          }}
+        >
+          Add Comment
+        </button>
+      </form>
     </div>
   );
 };
